c9169e95cc0e4910d267eccbc9037fb166c4008a
fperotto <filipo.perotto@onera.fr>
2025-03-18 13:22:33 +0100

    input and output dims
    Signed-off-by: fperotto <filipo.perotto@onera.fr>

--- src/onnx_utils.py
+++ src/onnx_utils.py
@@ -9,11 +9,15 @@ Created on MARCH 2025
 # ONNX UTILS
 ##########################################################
 
+#import numpy as np
+
+##########################################################
+
 def get_inputs(onnx_model):
     #the name of the input (e.g.: 'input', 'X', etc.)
     #nb_inputs = len(onnx_model.graph.input)
     onnx_input_nodes = []
-    for i, input_node in enumerate(onnx_model.graph.input):
+    for input_node in onnx_model.graph.input:
         onnx_input_nodes.append({"name":input_node.name, "dims": [dim.dim_value for dim in input_node.type.tensor_type.shape.dim]})
     #print("inputs:", onnx_input_nodes)
     return onnx_input_nodes
@@ -22,8 +26,19 @@ def get_outputs(onnx_model):
     #the name of the output (e.g.: 'ouput', 'Y', etc.)
     nb_outputs = len(onnx_model.graph.output)
     onnx_output_nodes = []
-    for i, output_node in enumerate(onnx_model.graph.output):
+    for output_node in onnx_model.graph.output:
         onnx_output_nodes.append({"name":output_node.name, "dims": [dim.dim_value for dim in output_node.type.tensor_type.shape.dim]})
     #print("outputs:", onnx_output_nodes)
     return onnx_output_nodes
     
+def get_input_dims(onnx_model):
+    onnx_input_dims = []
+    for input_node in onnx_model.graph.input:
+        onnx_input_dims.append([dim.dim_value for dim in input_node.type.tensor_type.shape.dim])
+    return onnx_input_dims
+
+def get_output_dims(onnx_model):
+    onnx_output_dims = []
+    for output_node in onnx_model.graph.output:
+        onnx_output_dims.append([dim.dim_value for dim in output_node.type.tensor_type.shape.dim])
+    return onnx_output_dims
